756651a4419dfe76846c4401ec991846f150e3a0
iga33087@gmail.com <iga33087@gmail.com>
2021-05-03 17:56:20 +0800

    vv

--- src/components/XccdfTreeItem2.vue
+++ src/components/XccdfTreeItem2.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="xccdfTreeItem2">
-    <el-dialog :title="data.name" :visible.sync="showDialog" width="50%">
-      <div>
-        <h2>Attributes<el-button type="success">Add Attributes</el-button></h2>
-        <el-form label-width="100px" v-if="data.attributes">
-          <template v-for="(item,index) in Object.keys(data.attributes)">
-            <el-form-item :label="item" :key="index" v-if="item!=='_ruleObj'">
-              <el-input v-model="data.attributes[item]" />
-              <span style="color:#f00;" v-if="!inputVerification(item).flag">!{{inputVerification(item).text}}</span>
-            </el-form-item>
-          </template>
-        </el-form>
+    <el-dialog :title="isAdd ? data.name+' - add':data.name+' - edit'" :visible.sync="showDialog" width="50%" v-if="showDialog">
+      <div v-if="isAdd">
+        <el-select v-model="chooseAdd">
+          <el-option v-for="(item,index) in Object.keys(getEleList)" :key="index" :label="item" :value="item"></el-option>
+        </el-select>
+        <div class="buttonList">
+          <el-button type="primary" @click="addEle">確定</el-button>
         </div>
-      <div class="buttonList">
-        <el-button type="primary">確定</el-button>
+      </div>
+      <div v-else>
+        <div>
+          <h2 class="xccdfTreeItem2DiaBoxH2">Attributes<el-button type="success">Add Attributes</el-button></h2>
+          <el-form label-width="100px" v-if="data.attributes">
+            <template v-for="(item,index) in Object.keys(data.attributes)">
+              <el-form-item :label="item" :key="index">
+                <el-input v-model="data.attributes[item]" />
+                <span style="color:#f00;" v-if="!inputVerification(item).flag">{{inputVerification(item).text}}</span>
+              </el-form-item>
+            </template>
+            <el-form-item :label="'內容'">
+              <el-input type="textarea" :rows="4"/>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="buttonList">
+          <el-button type="primary">確定</el-button>
+        </div>
       </div>
     </el-dialog>
     <div class="xccdfTreeItem2Box" @click="isOpen=!isOpen">
@@ -22,9 +35,9 @@
         {{data.name}}
       </div>
       <div class="xccdfTreeItem2BoxMenu">
-        <el-tag class="xccdfTreeItem2BoxMenuError" type="danger" v-if="getErrCount">ERROR x {{getErrCount}}</el-tag>
-        <el-button type="success" @click.stop="showDialog=!showDialog">新增</el-button>
-        <el-button type="warning" @click.stop="showDialog=!showDialog">修改</el-button>
+        <el-tag class="xccdfTreeItem2BoxMenuError" type="danger" v-if="getErrorCount">ERROR x {{getErrorCount}}</el-tag>
+        <el-button type="success" @click.stop="isAdd=true;showDialog=!showDialog">新增</el-button>
+        <el-button type="warning" @click.stop="isAdd=false;showDialog=!showDialog">修改</el-button>
         <el-button type="danger" @click.stop="showDialog=!showDialog">刪除</el-button>
       </div>
     </div>
@@ -35,6 +48,7 @@
 </template>
 
 <script>
+import convert  from 'xml-js'
 import XccdfTreeItem2 from '@/components/XccdfTreeItem2.vue'
 export default {
   name:"XccdfTreeItem2",
@@ -43,6 +57,8 @@ export default {
     return {
       isOpen:this.defaultIsOpen,
       showDialog:false,
+      isAdd:true,
+      chooseAdd:""
     }
   },
   components: {XccdfTreeItem2},
@@ -58,21 +74,38 @@ export default {
       }
       return res
     },
-    getErrCount() {
+    getEleList() {
+      let res={}
+      let arr=this.getflatEleListByTagName.filter(res=>res.category==='element')
+      for(let item of arr) {
+        res[item.tag]=item
+      }
+      return res
+    },
+    getErrorCount() {
       let res=0
-      for(let item of Object.keys(this.data.attributes)) {
-        let flag=this.inputVerification(item)
-        if(!flag.flag) {
-          res++
-        }
+      let arr=Object.keys(this.ruleObj)
+      if(!arr) return res
+      for(let item of arr) {
+        let obj=this.inputVerification(item)
+        if(!obj.flag) res++
       }
       return res
     }
   },
   methods: {
-    upData() {
+    addEle() {
+      //let obj=this.data
+      //obj.elements.push(this.chooseAdd)
+      let obj=this.data
+      let newEle=this.$global.createEle(this.chooseAdd).outerHTML
+      newEle=(convert.xml2js(newEle))['elements'][0]
+      obj.elements.push(newEle)
+      this.upData(obj)
+    },
+    upData(x=this.data) {
       let obj= {
-        value:this.data,
+        value:x,
         index:this.index
       }
       this.$emit('upData',obj)
@@ -80,7 +113,7 @@ export default {
     inputVerification(x) {
       if(!this.ruleObj||!this.ruleObj[x]) return {flag:true}
       if(this.ruleObj[x].count=='1'||this.ruleObj[x].count=='1-n') {
-        if(!this.data.attributes[x]) return {flag:false,text:"此欄位為必填"}
+        if(!this.data.attributes[x]) return {flag:false,text:"ERROE : 此欄位為必填!"}
         else return {flag:true}
       }
       else {
